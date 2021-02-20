7a39becd3281a9c31d4801ff51daa25f8da1c60e
Humberox <humberto.ninocr@uanl.edu.mx>
2021-02-19 20:39:41 -0600

    Create MercadoLibreScraping.py

---
+++ MercadoLibreScraping.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+Enlace = 'https://listado.mercadolibre.com.mx/telefonos-celulares#D[A:telefonos%20celulares]'
+page = requests.get(Enlace)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#Celulares
+Celu = soup.find_all('h2', class_='ui-search-item__title')
+Celulares = []
+count = 0
+for i in Celu:
+    if count < 30:
+        Celulares.append(i.text)
+    else:
+        break
+    count += 1
+#Precios
+Cos = soup.find_all('span', class_='price-tag-fraction')
+Costos = []
+count = 0
+for i in Cos:
+    if count < 30:
+        Costos.append(i.text)
+    else:
+        break
+    count += 1
+
+df = pd.DataFrame({'Nombre': Celulares, 'Precio': Costos}, index = list(range(1,31)))
+print(df)
+df.to_csv('Precios Totales.csv', index=False)
